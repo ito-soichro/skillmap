d50db5552349f1f079c8f9af8c1958c1759ea3a7
ito-soichro <queen.fm1235@gamil.com>
2020-05-09 20:50:47 +0900

    [新規作成]

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,10 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+
+Refile.secret_key = 'ace291161ae43c258c84bbdeb543edddf198b69c821f59cffdc3d906a3f7b95f6079bbec5af12a167594c806d52c3c330dbcfede0259237a3dfeed039f934629'
