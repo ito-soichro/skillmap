53cf37e1a8b56e07cd378490a4e58cf3d17f4912
ito-soichro <queen.fm1235@gamil.com>
2020-06-27 14:56:04 +0900

    検索機能の実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,12 @@ class User < ApplicationRecord
 
   attachment :profile_image      
   has_many :tweets, dependent: :destroy
-
+  has_many :comments
+  has_many :likes, dependent: :destroy
   validates :username, presence: true 
+
+  def already_liked?(tweet)
+    self.likes.exists?(tweet_id: tweet.id)
+  end
     
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,12 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "home#index"
   resources :users 
-  resources :tweets
-  
+  resources :tweets do 
+    resources :comments, only: :create
+      collection do
+        get 'search'
+      end
+    resource :likes, only: [:create, :destroy]
+  end
 
 end
